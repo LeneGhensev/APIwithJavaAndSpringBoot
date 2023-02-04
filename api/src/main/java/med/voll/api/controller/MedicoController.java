b9546dde8b58f4575aece8518c6e41aa00fc9231
Lene Ghensev <lenegghensev@gmail.com>
2023-02-03 22:24:16 -0300

    Validação com Bean Validation

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -16,13 +17,15 @@ import med.voll.api.medico.MedicoRepository;
 public class MedicoController {
 
     @Autowired //define o local onde quer que o Spring injete a dependência. O Spring entende que ele que irá instanciar esse atributo.
-    private MedicoRepository repository; //como precisou chamar essa classe no método, declarou-se o repository como atributo aqui.
+    private MedicoRepository repository; //como precisou chamar essa classe no método, declara-se o repository como atributo aqui.
     
     @PostMapping //o PostMapping mapeia requisições post para a url com /medicos
     // public void cadastrar(@RequestBody String json) - o RequestBody diz que os parâmetros devem ser lidos do corpo da requisição.
     // System.out.println(dados); Dessa forma ("String json" acima) ele trás todos os campos juntos como uma única String, o ideia então é criar uma classe para separar os parâmetros
     @Transactional //por ser um método de escrita, precisa ter transação ativa com o banco de dados.
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {    
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {    
         repository.save(new Medico(dados)); //os parâmetros que estão vindo da requisição serão passados aqui.
+        //esses dados é do construtor que está na classe Medico
+        // adiciona o @Valid para se integrar com o bean validation e fazer as validações
     }
 }
--- api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,7 +1,31 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+//O Bean Validation, a partir das anotações, vai verificar se as informações que chegam estão de acordo com as anotações.
+
+public record DadosCadastroMedico(
+    @NotBlank //informa que é um campo obrigatório, não pode ser nulo e nem vazio  
+    String nome,
+
+    @NotBlank //informa que é um campo obrigatório, não pode ser nulo e nem vazio  
+    @Email
+    String email, 
+    
+    @NotBlank
+    @Pattern(regexp = "\\d{4,6}") //informa que deverá ser um número de 4 a 6 dígitos
+    String crm, 
+
+    @NotNull //informa que não pode ser nulo e nem vazio  
+    Especialidade especialidade, 
+
+    @NotNull
+    @Valid //para fazer a validação do dto
+    DadosEndereco endereco) {
     // O record faz o Java criar uma classe imutável, em que cada um desses campos vai virar atributos com os métodos getters e setters e com os construtores, sem precisarmos fazer isso manualmente pois o Spring cria automaticamente. 
 }
